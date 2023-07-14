4af4de35bdd4b0f1f2c5b0ced0e457f190062c6d
yeji <104903908+dhEhdod@users.noreply.github.com>
2023-07-14 21:28:58 +0900

    Update main.py

--- code/group4/main.py
+++ code/group4/main.py
@@ -40,48 +40,6 @@ def get_round_info(Round_num: str):
     return crud.read_round_info(Round_num)
 
 
-
-
-
-
-'''
-class Vote(BaseModel):
-    selected_game : str
-    selected_game_num : int
-'''
-        
-    
-@app.post("/Vote")
-def get_vote(selected_game: Annotated[str, Body()]):
-    print(f"{selected_game}번 경기가 선택되었습니다")
-    selected_game = int(selected_game)
-    global count1, count2, count3, count4, count5, count6
-    if selected_game == 1:
-        count1 += 1
-    elif selected_game == 2:
-        count2 += 1
-    elif selected_game == 3:
-        count3 += 1
-    elif selected_game == 4:
-        count4 += 1
-    elif selected_game == 5:
-        count5 += 1
-    elif selected_game == 6:
-        count6 += 1
-        
-    count = count1 + count2 + count3 + count4 + count5 + count6
-    
-    rate1 = round(count1 / count * 100, 2)
-    rate2 = round(count2 / count * 100, 2)
-    rate3 = round(count3 / count * 100, 2)
-    rate4 = round(count4 / count * 100, 2)
-    rate5 = round(count5 / count * 100, 2)
-    rate6 = round(count6 / count * 100, 2)
-    
-    response = (f"<{selected_round}라운드 득표 현황> 1경기: {rate1}% / 2경기: {rate2}% / 3경기: {rate3}% / 4경기: {rate4}% / 5경기: {rate5}% / 6경기: {rate6}% / select: {selected_game}")
-    return {response}
-
-
     
 @app.post("/CountUp")
 def show_vote_count(Round_num: Annotated[str, Body()], selected_game: Annotated[int, Body()]):
@@ -95,5 +53,3 @@ def show_vote_count(Round_num: Annotated[str, Body()], selected_game: Annotated[
 @app.get("/RoundPeriod")
 def get_round_period():
     return crud.read_round_period()
-
-
