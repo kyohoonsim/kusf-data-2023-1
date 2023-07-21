4eb631d485c2deda84d1f93bbb9d60b7cca632f5
yeji <104903908+dhEhdod@users.noreply.github.com>
2023-07-21 09:29:56 +0900

    Update main.py

--- code/group4/main.py
+++ code/group4/main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI, Body, Header
 from typing import Union
 from typing_extensions import Annotated
 from pydantic import BaseModel
+from datetime import datetime
 from fastapi.middleware.cors import CORSMiddleware
 
+
+import pytz
 import crud
 
 selected_round = None  # 전역 변수 선언
@@ -25,16 +28,15 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+@app.get("/Round")
+def get_round():
+    return crud.read_round()
+
+
 
 @app.get("/RoundPeriod")
 def get_round_period():
     return crud.read_round_period()
-
-
-
-@app.get("/Round")
-def get_round():
-    return crud.read_round()
 
 
 
@@ -55,5 +57,28 @@ def show_vote_count(Round_num: Annotated[str, Body()], selected_game: Annotated[
     
     return crud.update_vote_count(Round_num, Game_num)
 
+
+
+
+
+
+# 서버의 기본 시간대를 UTC로 설정합니다.
+server_timezone = pytz.timezone("UTC")
+
+@app.get("/getServerTime")
+
+def get_server_time():
+    server_time = datetime.now().astimezone(server_timezone)
+    # 한국 시간대로 변경하기
+    korea_timezone = pytz.timezone("Asia/Seoul")
+    server_time_korea = server_time.astimezone(korea_timezone)
+
+
+    # 년, 월, 일만 추출하기
+    year = [server_time_korea.year]
+    month = [server_time_korea.month]
+    day = [server_time_korea.day]
+
+    return {"year": year, "month": month, "day": day}
 
 
